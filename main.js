17375caaaca925f28a779264e310f1c42c820309
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2021-01-19 19:43:02 +0100

    try to modify something

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ module.exports.loop = function () {
     for(var name in Memory.creeps) {
         if(!Game.creeps[name]) {
             delete Memory.creeps[name];
-            console.log('Clearing non-existing creep memoy:', name);
+            console.log('Clearing non-existing creep memorry:', name);
         }
     }
 
