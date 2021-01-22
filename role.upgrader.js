be1eb1eef77eb5acbba7c733611a2ef6b4ca761b
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2021-01-22 12:01:46 +0100

    use the architecture of the builder from the documentation for the upgrader

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,25 +2,27 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-        
-        creep.say(creep.store.getUsedCapacity());
-        if(creep.store.getUsedCapacity() < 1) {
-            //creep.say(creep.store.getUsedCapacity());
-	        creep.say("> source");
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
-                //console.log('Upgrader: ' + creep.name + ' will move to source');
-            }
-        }
-        else if(creep.store.getUsedCapacity() > 1){
-            creep.say("> controller");
-            var sources_controller_object = creep.room.controller;
+         if(creep.memory.building && creep.store[RESOURCE_ENERGY] == 0) {
+            creep.memory.building = false;
+            creep.say('🔄 harvest');
+	    }
+	    if(!creep.memory.building && creep.store.getFreeCapacity() == 0) {
+	        creep.memory.building = true;
+	        creep.say('🚧 upgrade');
+	    }
+
+	    if(creep.memory.building) {
+	        var sources_controller_object = creep.room.controller;
             if(creep.upgradeController(sources_controller_object, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources_controller_object);
-                //console.log('Upgrader: ' + creep.name + ' will move to controller');
             }
-        }
+	    }
+	    else {
+	        var sources = creep.room.find(FIND_SOURCES);
+            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+	    }
 	}
 };
 
