f53412f6a2cb399e8f22c64add987e5a993add9b
Pierre Geeraert <pierre.geeraert@bbconsult.co.uk>
2021-01-22 11:47:33 +0100

    try some tests but not really working

--- role.upgrader.js
+++ role.upgrader.js
@@ -2,14 +2,19 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.store.getFreeCapacity() > 99) {
+        
+        creep.say(creep.store.getUsedCapacity());
+        if(creep.store.getUsedCapacity() < 1) {
+            //creep.say(creep.store.getUsedCapacity());
+	        creep.say("> source");
             var sources = creep.room.find(FIND_SOURCES);
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0]);
                 //console.log('Upgrader: ' + creep.name + ' will move to source');
             }
         }
-        else {
+        else if(creep.store.getUsedCapacity() > 1){
+            creep.say("> controller");
             var sources_controller_object = creep.room.controller;
             if(creep.upgradeController(sources_controller_object, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources_controller_object);
